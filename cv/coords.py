5a82f21ff8f62df33a1dfd280ba86604a2195c5f
marcusbarbu <mgb.barbu@gmail.com>
2019-02-10 14:31:40 -0500

    Added room for code prototypes

--- cv/coords.py
+++ cv/coords.py
@@ -27,12 +27,12 @@ def recolor_frames(frames):
 def get_coordinates(frames, detector, cam_properties, colored_frames):
     if len(frames) != 2:
         return None
-    x_left, y_left = detector.detect(frames[1])[0].center
-    x_right, y_right = detector.detect(frames[0])[0].center
+    x_left, y_left = detector.detect(frames[0])[0].center
+    x_right, y_right = detector.detect(frames[1])[0].center
     print "x_left %f x_right %f b %f f %f" % (x_left, x_right, cam_properties.b, cam_properties.f)
     
-    colored_frames[1] = cv2.circle(colored_frames[1], (int(x_left), int(y_left)), 15, (0, 0, 255), -1)
-    colored_frames[0] = cv2.circle(colored_frames[0], (int(x_right), int(y_right)), 15, (255, 0, 0), -1)
+    colored_frames[0] = cv2.circle(colored_frames[0], (int(x_left), int(y_left)), 15, (0, 0, 255), -1)
+    colored_frames[1] = cv2.circle(colored_frames[1], (int(x_right), int(y_right)), 15, (255, 0, 0), -1)
 
     x_left = abs(960 - x_left)
     x_right = abs(960 - x_right)
@@ -51,13 +51,15 @@ def scale(coord):
     #return coord[0] * 150, coords[1] * 60000000
     x = coord[0]
     y = coord[1] 
-    x = 400 + (x * 150)
-    y = 400 + (y *60000000)
+    x = 150 - (x * 150)
+    y = 300 - (y * 80000000)
+    x *= 3
+    y *= 3
     return x, y
 
 def display_coords(coords, t):
     x, y = scale(coords)
-    print ("x: %f -- z: %f" % (coords[0], coords[1]))
+    print ("x: %f -- z: %.10f" % (coords[0], coords[1]))
     print ("scaled x: %f -- z: %f" % (x, y))
     t.goto(x, y) 
 
@@ -65,7 +67,7 @@ if __name__ == "__main__":
     t = turtle.Turtle()
     t.color("purple")
     t.width(5)
-    props = CameraProperties(0.093, 0.002)
+    props = CameraProperties(0.094, 0.0025)
     print props.b
     print "b %f f %f" % (props.b, props.f)
     detector = apriltag.Detector() 
@@ -82,10 +84,10 @@ if __name__ == "__main__":
             except Exception as e:
                 #print e
                 pass
-            cv2.imshow('right', orig_frames[1])
-            cv2.imshow('left', orig_frames[0])
-            if cv2.waitKey(1) & 0xff == ord('q'):
-                close(cams)
+            #cv2.imshow('right', orig_frames[0])
+            #cv2.imshow('left', orig_frames[1])
+            #if cv2.waitKey(1) & 0xff == ord('q'):
+            #    close(cams)
             if coords is not None:
                 display_coords(coords, t)
             time.sleep(0.01)
