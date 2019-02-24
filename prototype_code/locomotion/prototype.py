ed7d3b3e50d5ad66cbf46af79e5593cdb6069740
Jack Zheng <jackzheng@nyu.edu>
2019-02-23 20:53:15 -0500

    Another update

--- prototype_code/locomotion/prototype.py
+++ prototype_code/locomotion/prototype.py
@@ -1,26 +1,25 @@
-from ComputerVision import ComputerVision
+# from ComputerVision import ComputerVision
 import time
 
 class CameraProperties:
 
-	def __init__(self. b, f):
+	def __init__(self, b, f):
 		self.b = b # Focal length, need to figure out
 		self.f = f # Distance between the cameras
 
 class Locomotion:
-
     def __init__(self):
-        self.Flag_Begin = 1
+    	self.Flag_Begin = 1
         self.Flag_Collection = 0
         self.Flag_Deposition = 0
 
-	def disengage():
+	def disengage(self):
 		# TODO: Disengage drummer from robot
 		print "Disengaging drummer from robot \n"
 		self.Flag_Begin = 0
 		self.Flag_Collection = 1
 
-	def align_depBin():
+	def align_depBin(self):
 		aligned = False
 		while not aligned:
 			# TODO: Align the robot to the deposition bin
@@ -33,7 +32,7 @@ class Locomotion:
 		self.Flag_Collection = 1
 
 
-	def align_drummer():
+	def align_drummer(self):
 		aligned = False
 		while not aligned:
 			# TODO: Align the robot with the drummer
@@ -44,7 +43,7 @@ class Locomotion:
 		self.Flag_Deposition = 1
 		self.Flag_Collection = 0
 
-	def do_task():
+	def do_task(self):
 		if self.Flag_Begin:
 			disengage()
 		elif self.Flag_Collection:
@@ -52,43 +51,29 @@ class Locomotion:
 		elif self.Flag_Deposition:
 			align_depBin()
 
-	def autopilot(coords):
+	def autopilot(self, coords):
 		arrived = False
 		while arrived:
 			# TODO: Move toward the coordinates
 			print "Moving towards " + coords
+			time.sleep(2)
 			arrived = True
 		do_task()
 
-if __name__ == "__main__":
+	def get_coordinates(self):
+		if self.Flag_Begin:
+			return (12, 12) # TODO: Get coords of digging location using apriltag and CV
+		elif self.Flag_Collection:
+			return (12, 12) # TODO: Coords of digging location
+		elif self.Flag_Deposition:
+			return (0, 0) # TODO: Coords for the deposition bin
 
-    loc = Locomotion()
-	apriltag_coords = [12, 12] # TODO: Get coords of digging location using apriltag and CV
-	bin_coords = [0, 0] # TODO: Coords for the deposition bin
+if __name__ == "__main__":
+	print "hello"
+	loc = Locomotion()
 	try:
 		while True:
-
+			coords = loc.get_coordinates()
+			loc.autopilot(coords)
 	except KeyboardInterrupt:
 		pass
-
-
-
-	# cam0 = cv2.VideoCapture(0)
-	# cam1 = cv2.VideoCapture(1)
-	# cams = [cam0, cam1]
-	# detector = apriltag.Detector()
-	# props = CameraProperties(0.094, 0.0025)
-	# cv = ComputerVision()
-
-	# try:
-	# 	while True:
-	# 		orig_frames = cv.get_frames(cams)
-	# 		gray_frames = cv.recolor_frames(orig_frames)
-	# 		coords = None
-	# 		try:
-	# 			coords = cv.get_coordinates(gray_frames, detector, props, orig_frames)
-	# 		except Exception as e:
-	# 			pass
-	#
-	# except KeyboardInterrupt:
-	# 	cv.close(cams)
