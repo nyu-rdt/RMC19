694adfad06e893b20e4f07826f1ab21c094fe693
marcusbarbu <mgb.barbu@gmail.com>
2019-04-21 14:32:59 -0400

    fixed beach code

--- beach_tester.py
+++ beach_tester.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import paho.mqtt.client as mqtt
 import time
 import sys
@@ -7,11 +9,13 @@ motors = ['motors/a', 'motors/b', 'linearActuator/a', 'linearActuator/b']
 
 
 m = motors[int(sys.argv[1])]
-power = int(int(sys.argv[2])/100) * 255)
+power = int(int(sys.argv[2])+100/200 * 255)
 client = mqtt.Client()
 client.connect('127.0.0.1')
 
 client.publish("digging/{}".format(m), payload=power, qos=0, retain=False)
+print("motor: {} power: {}".format(m, power))
 if (int(sys.argv[3]) == 1):
     time.sleep(0.5)
     client.publish("digging/{}".format(m), payload=0x7F, qos=0, retain=False)
+    print("stopping")
