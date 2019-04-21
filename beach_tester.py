41d709d1650906edf6395a9cd31ca2b373db583d
marcusbarbu <mgb.barbu@gmail.com>
2019-04-21 14:28:32 -0400

    added beach day code

---
+++ beach_tester.py
@@ -0,0 +1,17 @@
+import paho.mqtt.client as mqtt
+import time
+import sys
+
+assert(len(sys.argv) >= 2)
+motors = ['motors/a', 'motors/b', 'linearActuator/a', 'linearActuator/b']
+
+
+m = motors[int(sys.argv[1])]
+power = int(int(sys.argv[2])/100) * 255)
+client = mqtt.Client()
+client.connect('127.0.0.1')
+
+client.publish("digging/{}".format(m), payload=power, qos=0, retain=False)
+if (int(sys.argv[3]) == 1):
+    time.sleep(0.5)
+    client.publish("digging/{}".format(m), payload=0x7F, qos=0, retain=False)
