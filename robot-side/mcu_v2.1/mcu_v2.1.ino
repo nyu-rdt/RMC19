1fe55484417d77f701de0e1f9eca8db3eaab0f16
Theodore Kim <tk1931@nyu.edu>
2019-04-29 14:20:55 -0400

    MCU Basic Threading

---
+++ robot-side/mcu_v2.1/mcu_v2.1.ino
@@ -0,0 +1,304 @@
+// Version 2 of the TEENSY CODE
+// To get around the latencies of the previous version of the code
+// this version uses a interrupt driven code. Essentially, the main
+// loop is continually updating sensor values and when the Teensy
+// receives data from the ESP, it interrupts the sensor reading and
+// updates the motors. 
+// Measured latency: UNKNOWN (yet to be tested)
+
+// Communications Codes (rover agnostic)
+// COMMANDS
+#define WRITE_OUTPUT        0xF0
+#define READ_INPUT          0xF1
+#define READ_OUTPUT         0xF2
+#define REPLY_SENSOR        0xF3
+#define REPLY_MOTOR         0xF4
+#define PANIC               0xF5
+#define STOP                0xFF
+// OUTPUT ARGUMENTS
+#define GREEN_MOTOR_CHAN_1  0x01
+#define BLUE_MOTOR_CHAN_1   0x02
+#define BLUE_MOTOR_CHAN_2   0x04
+#define GOLD_MOTOR_CHAN_1   0x08
+#define GOLD_MOTOR_CHAN_2   0x10
+#define SERVO_MOTOR         0x20
+#define VIBRATION_MOTOR     0x40
+#define DEBUG_LED_ENABLE    0x80
+#define NUM_OUTPUTS         8
+// INPUT ARGUMENTS
+#define PULLUP_1            0x01
+#define PULLUP_2            0x02
+#define PULLUP_3            0x04
+#define PULLUP_4            0x08
+#define PULLUP_5            0x10
+#define PULLUP_6            0x20
+#define LINEAR_POT          0x40
+#define NUM_INPUT           8
+// ARGUMENT LENGTH
+#define ARGUMENT_LENGTH     0x80
+
+// constants for parse phase (define byte types)
+#define MOTOR_TYPE            1
+#define SENSOR_TYPE           2
+#define VALUE_TYPE            3
+#define STOP_TYPE             4
+#define START_TYPE            5
+
+// Pin Mappings
+// Motors
+int GreenMotorPWM = 3;
+int PurpleMotorPWM = 4;
+int BlueMotorPWM = 4;
+int GoldMotorPWM = 5;
+int BlackMotorPWM = 5;
+int VibrationMotorEnable = 20;
+int ServoPWM = 23;
+// Sensors
+int LinearPot = 14;
+int Pullup1 = 15;
+int Pullup2 = 16;
+int Pullup3 = A5;
+int Pullup4 = 25;
+int Pullup5 = 26;
+int Pullup6 = 12;
+int MiscSensor = A14;
+// Misc
+int DebugLED = 13;
+
+// Serial definitions
+HardwareSerial ESP = Serial1; // ESP WiFi Module
+HardwareSerial GoldMotor = Serial2;
+HardwareSerial BlueMotor = Serial3;
+#define ESP_BAUDRATE        115200
+#define ROBOTEQ_BAUDRATE    115200
+
+// These constants set whether or not the system should use SERIAL
+// or PWM to write to the motor outputs (BLUE and GOLD motors)
+#define USE_SERIAL          1
+#define USE_PWM             2
+
+// Buffers
+// pinMappings holds the mapings between the comm code and the pin
+void *pinMappings[ARGUMENT_LENGTH];
+// motorBuffer holds the current status of each motor
+int motorBuffer[ARGUMENT_LENGTH];
+// sensorBuffer holds the current status of each sensor
+int sensorBuffer[ARGUMENT_LENGTH];
+
+// Function prototypes
+int commandRoutine();
+int commandRoutineResource();
+int sensorRoutine(); 
+
+// Threads!
+#define T_STATUS_WAITING    0
+#define T_STATUS_BLOCKED    1
+#define T_STATUS_ACTIVE     2
+
+#define T_PRIORITY_HIGH     2
+#define T_PRIORITY_LOW      1
+
+struct PriorityThread {
+  int id;
+  int (*routine)();
+  int priority;
+  int threadStatus;
+  int (*resource)();
+};
+
+struct PriorityThread controller = {
+  0,
+  &commandRoutine,
+  T_PRIORITY_HIGH,
+  T_STATUS_WAITING,
+  &commandRoutineResource
+};
+
+struct PriorityThread sensor = {
+  0,
+  &sensorRoutine,
+  T_PRIORITY_LOW,
+  T_STATUS_WAITING,
+  NULL
+};
+
+struct PriorityThread threads[2] = { controller, sensor };
+
+void setup() {
+  // Begin listening on serial ports
+  ESP.begin(ESP_BAUDRATE);
+  BlueMotor.begin(ROBOTEQ_BAUDRATE);
+  GoldMotor.begin(ROBOTEQ_BAUDRATE);
+
+  // Pin modes
+  pinMode(Pullup1, INPUT);
+  pinMode(Pullup2, INPUT);
+  pinMode(Pullup3, INPUT);
+  pinMode(Pullup4, INPUT);
+  pinMode(Pullup5, INPUT);
+  pinMode(Pullup6, INPUT);
+  pinMode(DebugLED, OUTPUT);
+  pinMode(VibrationMotorEnable, OUTPUT);
+
+  // Initialize motorBuffer to 0
+  memset(motorBuffer, 0, ARGUMENT_LENGTH);
+  // Initialize sensorBuffer to all 0
+  memset(sensorBuffer, 0, ARGUMENT_LENGTH);
+  // Initialize pinMappings to all 0
+  memset(pinMappings, 0, ARGUMENT_LENGTH);
+  // Map pins, replace argument with USE_PWM to control motors with PWM
+  mapPins(USE_SERIAL);
+}
+
+void loop() {
+  // All the main loop does is update the sensors
+  int clockCounter = millis();
+  for (int i = 0; i < 2; ++i) {
+    if ((threads[i].threadStatus == T_STATUS_WAITING) || (threads[i].resource() > 0)) {
+      runThread(threads[i]);
+    }
+  }
+}
+
+void runThread(struct PriorityThread *thread) {
+  thread->threadStatus = T_STATUS_ACTIVE;
+
+  if (thread->routine() < 0) {
+    thread->threadStatus = T_STATUS_BLOCKED;
+  }
+  else {
+    thread->threadStatus = T_STATUS_WAITING;
+  }
+}
+
+// This function (run in a loop) continually updates all of the sensors
+int sensorRoutine () {
+  return 1;
+}
+
+int commandRoutineResource() {
+  return (ESP.available() > 0);
+}
+
+// This function (run in a loop) continually listens for commands
+int commandRoutine () {
+  static int nextByteType = START_TYPE; // What is the next byte to expect?
+  static int motorIdentifier = 0; // What motor to expect a value for?
+  static int sensorIdentifier = 0; // What motor to expect a value for?
+
+  // For each byte in the read buffer
+  if (ESP.available() > 0) {
+    int currentByte = ESP.read();
+    
+    if (nextByteType == START_TYPE && currentByte == WRITE_OUTPUT) {
+      nextByteType = MOTOR_TYPE;
+    }
+    else if (nextByteType == START_TYPE && currentByte == READ_INPUT) {
+      nextByteType = SENSOR_TYPE;
+    }
+    else if (nextByteType == START_TYPE && currentByte == PANIC) {
+      panic();
+    }
+    else if (nextByteType == MOTOR_TYPE) {
+      motorIdentifier = currentByte;
+      nextByteType = VALUE_TYPE;
+    }
+    else if (nextByteType == SENSOR_TYPE) {
+      // Enable sensor read
+      sensorIdentifier = currentByte;
+      nextByteType = STOP_TYPE;
+    }
+    else if (nextByteType == VALUE_TYPE) {
+      // Set motor speed
+      motorBuffer[motorIdentifier] = currentByte;
+      nextByteType = STOP_TYPE;
+    }
+    else if (nextByteType == STOP_TYPE && currentByte == STOP){
+      nextByteType = START_TYPE;
+      commit(motorIdentifier, sensorIdentifier, motorBuffer[motorIdentifier]);
+      motorIdentifier = 0;
+      sensorIdentifier = 0;
+    }
+    else {
+      nextByteType = START_TYPE;
+    }
+
+    return 1;
+  }
+  
+  return 0;
+}
+
+// This function commits changes to the robot state
+void commit(int motor, int sensor, int value) {
+  if (motor != 0) {
+    // A motor
+    writeMotor(motor, value);
+  }
+  else if (sensor != 0) {
+    // A sensor 
+    readSensor();
+  }
+}
+
+// Write a value to a motor
+void writeMotor(int motorNum, int power) {
+  void *motor = pinMappings[motorNum];
+  if ((HardwareSerial *)motor == &BlueMotor || (HardwareSerial *)motor == &GoldMotor) {
+    int signalSerial = (power != 0) * ((power - 128) * 3937);
+    String chan = String((((motorNum == BLUE_MOTOR_CHAN_2) || (motorNum == GOLD_MOTOR_CHAN_2)) + 1));
+    String power = String(signalSerial);
+    (*(HardwareSerial *)motor).println("!G " + chan + " " + power + "\r");
+  }
+  else {
+    // Write to the serial port
+    unsigned int signalPWM = (unsigned long)power * 205 / 255 + 204; // Equation to map from 0-255 ---> 204-409 (12-bit resolution)
+    analogWrite(*(int *)motor, signalPWM);
+  }
+}
+
+void readSensor() {
+  // Not yet implemented.
+}
+
+// This functions sends a reply to the ESPs
+int reply(int replyCode, int value) {
+  // TODO 
+}
+
+// This function stops ALL OUTPUTS. Used during disconnect
+void panic() {
+  for (int i = 0; i < NUM_OUTPUTS; ++i) {
+    writeMotor(1 << i, 0);
+  }
+}
+
+// This function maps the communication codes to the pins
+void mapPins(int isSerial) {
+  if (isSerial == USE_SERIAL) {
+    pinMappings[BLUE_MOTOR_CHAN_1] = (void *)&BlueMotor;
+    pinMappings[BLUE_MOTOR_CHAN_2] = (void *)&BlueMotor;
+    pinMappings[GOLD_MOTOR_CHAN_1] = (void *)&GoldMotor;
+    pinMappings[GOLD_MOTOR_CHAN_2] = (void *)&GoldMotor;
+  }
+  else {
+    // Multiple channels are not supported using PWM
+    pinMappings[BLUE_MOTOR_CHAN_1] = (void *)&BlueMotorPWM;
+    pinMappings[BLUE_MOTOR_CHAN_2] = 0;
+    pinMappings[GOLD_MOTOR_CHAN_1] = (void *)&GoldMotorPWM;
+    pinMappings[GOLD_MOTOR_CHAN_2] = 0;
+  }
+  // Normal output mappings
+  pinMappings[SERVO_MOTOR] = (void *)&ServoPWM;
+  pinMappings[VIBRATION_MOTOR] = (void *)&VibrationMotorEnable;
+  pinMappings[DEBUG_LED_ENABLE] = (void *)&DebugLED;
+  pinMappings[GREEN_MOTOR_CHAN_1] = (void *)&GreenMotorPWM;
+  // Sensor mappings (XORed by 0xFF)
+  pinMappings[0xFF ^ PULLUP_1] = (void *)&Pullup1;
+  pinMappings[0xFF ^ PULLUP_2] = (void *)&Pullup2;
+  pinMappings[0xFF ^ PULLUP_3] = (void *)&Pullup3;
+  pinMappings[0xFF ^ PULLUP_4] = (void *)&Pullup4;
+  pinMappings[0xFF ^ PULLUP_5] = (void *)&Pullup5;
+  pinMappings[0xFF ^ PULLUP_6] = (void *)&Pullup6;
+  pinMappings[0xFF ^ LINEAR_POT] = (void *)&LinearPot;
+}
---
+++ robot-side/mcu_v2/README.md
@@ -0,0 +1,22 @@
+PIN MAPPINGS:
+
+Locomotion:
+
+- Front Right: BLUE MOTOR CHANNEL 1: 0x02
+- Front Left: GOLD MOTOR CHANNEL 1: 0x08
+- Back Right: BLUE MOTOR CHANNEL 2: 0x04
+- Back Left: GOLD MOTOR CHANNEL 2: 0x10
+
+Deposition:
+- Linear Actuator: GREEN MOTOR: 0x01
+- Left Motor: BLUE MOTOR CHANNEL 1: 0x02
+- Right Motor: GOLD MOTOR CHANNEL 1: 0x08
+- Vibration Motors: VIBRATION MOTOR ENABLE: 0x40
+
+Digging:
+- Drum 1: BLUE MOTOR CHANNEL 1: 0x02
+- Drum 2: GOLD MOTOR CHANNEL 1: 0x08
+- Linear Actuator: GREEN MOTOR: 0x01
+- Door Actuator: SERVO MOTOR: 0x20
+
+Also the debug LED can be turned on using 0x80 (DEBUG LED)
