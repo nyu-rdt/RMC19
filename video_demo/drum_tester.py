ecf3cf00bff2b5e9a2d06b038b725223ef01cf65
marcusbarbu <mgb.barbu@gmail.com>
2019-04-29 20:57:57 -0700

    added runners for demo

---
+++ video_demo/drum_tester.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import paho.mqtt.client as mqtt
+import time
+import sys
+
+assert(len(sys.argv) >= 2)
+motors = ['', 'motors/a', 'motors/b', 'linearActuator/a', 'linearActuator/b']
+
+
+m1 = 'motors/a'
+m2 = 'motors/b'
+#power = int((int(sys.argv[2])+100)/200 * 255)
+power = int(sys.argv[2])
+client = mqtt.Client()
+client.connect('127.0.0.1')
+
+client.publish("digging/{}".format(m1), payload=power, qos=0, retain=False)
+time.sleep(0.01)
+client.publish("digging/{}".format(m2), payload=power, qos=0, retain=False)
+time.sleep(0.01)
+client.publish("digging/{}".format(m2), payload=power, qos=0, retain=False)
+print("motor: {} power: {}".format(m1, power))
+print("motor: {} power: {}".format(m2, power))
+if (len(sys.argv) >3):
+    if (int(sys.argv[3]) == 1):
+        time.sleep(0.5)
+        client.publish("digging/{}".format(m1), payload=0x00, qos=0, retain=False)
+        client.publish("digging/{}".format(m2), payload=0x00, qos=0, retain=False)
+        print("stopping")
